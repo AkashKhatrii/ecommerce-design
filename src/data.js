fe26bcd22afc585e676c522ec9f233169f00fa16
Akash Khatri <akashkhatri1001@gmail.com>
2022-02-28 01:01:14 +0530

    Initial Commit

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://indian-retailer.s3.ap-south-1.amazonaws.com/s3fs-public/2021-05/revenge%20shopping.jpg",
+    title: "SUMMER SALE",
+    desc: "DONT COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://indian-retailer.s3.ap-south-1.amazonaws.com/s3fs-public/2021-05/revenge%20shopping.jpg",
+    title: "AUTUMN SALE",
+    desc: "DONT COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://indian-retailer.s3.ap-south-1.amazonaws.com/s3fs-public/2021-05/revenge%20shopping.jpg",
+    title: "WINTER SALE",
+    desc: "DONT COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+];
